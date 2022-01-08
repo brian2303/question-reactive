fcb602d50ea4a9411be45ffc1f170759f1f1d928
brihdz <brayan_andrey23@hotmail.com>
2022-01-07 22:55:03 -0500

    mostrar botones de calificacion de respuesta solo si esta autenticado

--- web/src/components/answer/Answer.js
+++ web/src/components/answer/Answer.js
@@ -3,19 +3,27 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 import './answer.css'
+import { connect } from 'react-redux';
 
 
-export const Answer = ({ answer }) => {
+const Answer = ({ answer,userId }) => {
   const handleClick = () => {
     console.log('click');
   }
   return (
     <aside className='answer'>
       <p className='answer-content'>{answer.answer}</p>
-      <div className='score-button'>
-        <KeyboardArrowUpIcon className='keyboard' onClick={handleClick}/>
-        <KeyboardArrowDownIcon className='keyboard' onClick={handleClick}/>
-      </div>
+      {userId &&
+        <div className='score-button'>
+          <KeyboardArrowUpIcon className='keyboard' onClick={handleClick}/>
+          <KeyboardArrowDownIcon className='keyboard' onClick={handleClick}/>
+        </div>}
     </aside>
   )
 }
+
+const mapStateToProps = state => ({
+  userId: state.auth.uid
+})
+
+export default connect(mapStateToProps)(Answer)
